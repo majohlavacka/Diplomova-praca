e99659135ec40a01120711c162610a447c21b188
Majo <91958835+majohlavacka@users.noreply.github.com>
2025-09-21 16:50:04 +0200

    Update ikmu.sh

--- ikmu.sh
+++ ikmu.sh
@@ -3,7 +3,7 @@
 clear
 
 show_help() {
-    echo "Pouzitie: ./bruni alebo bash bruni"
+    echo "Pouzitie: ./ikmu alebo bash ikmu"
     echo ""
     echo "  -h, --help       Zobrazi napovedu pouzitia nastroja"
     echo ""
@@ -21,42 +21,45 @@ show_help() {
     exit 0
 }
 
-# support ./bruteuni -h/--help 
+menu() {
+    echo "###################################"
+    echo "#           IKnowMyUni            #"
+    echo "###################################"
+    echo
+    echo "[*] Dostupne moznosti:"
+    echo
+    echo "[a] Testovanie odozvy a Rate-limitingu"
+    echo "[b] Generovat hesla"
+    echo "[c] Brute-force na UKF Webmail"
+    echo "[d] Brute-force na UKF AiS"
+    echo "[e] Zistenie HTTP hlaviciek"
+    echo "[f] Zistenie technologii"
+    echo "[g] Zistenie IP adresy a polohy"
+    echo "[i] Hladat heslo vo vytvorenom zozname"
+    echo "[h] Napoveda"
+    echo "[x] Ukoncit nastroj"
+    echo
+    read -p "[*] Moznost: " choice
+
+    case "$choice" in
+        a) /home/kali/diplom/modules/ping-uni.sh ;;
+        b) /home/kali/diplom/modules/gen-pass.sh ;;
+        c) python3 /home/kali/diplom/modules/brute-webmail.py ;;
+        d) python3 /home/kali/diplom/modules/brute-ais.py ;;
+        e) /home/kali/diplom/modules/http-headers.sh ;;
+        f) /home/kali/diplom/modules/tech-detect.sh ;;
+        g) /home/kali/diplom/modules/ip-info.sh ;;
+        i) /home/kali/diplom/modules/find-pass.sh ;;
+        h|-h|--help) show_help ;;
+        x) exit 0 ;;
+        *) echo "Zadali ste nespravnu moznost. Koniec programu."; exit 1 ;;
+    esac
+}
+
+# support ./ikmu -h/--help
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     show_help
 fi
 
-# menu
-echo "###################################"
-echo "#           IKnowMyUni            #"
-echo "###################################"
-echo
-echo "[*] Dostupne moznosti:"
-echo
-echo "[a] Testovanie odozvy a Rate-limitingu"
-echo "[b] Generovat hesla"
-echo "[c] Brute-force na UKF Webmail"
-echo "[d] Brute-force na UKF AiS"
-echo "[e] Zistenie HTTP hlaviciek"
-echo "[f] Zistenie technologii"
-echo "[g] Zistenie IP adresy a polohy"
-echo "[i] Hladat heslo vo vytvorenom zozname"
-echo "[h] Napoveda"
-echo "[x] Ukoncit nastroj"
-
-echo
-read -p "[*] Moznost: " choice
-
-case "$choice" in
-  a) modules/ping-uni.sh ;;
-  b) modules/gen-pass.sh ;;
-  c) python3 modules/brute-webmail.py ;;
-  d) modules/brute-ais.py ;;
-  e) modules/http-headers.sh ;;
-  f) modules/tech-detect.sh ;;
-  g) modules/ip-info.sh ;;
-  i) modules/find-pass.sh ;;
-  h|-h|--help) show_help ;;   
-  x) exit 0 ;;
-  *) echo "Zadali ste nespravnu moznost. Koniec programu."; exit 1 ;;
-esac
+# zavolanie menu
+menu
